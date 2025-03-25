418923267fe773c5a5bff03c641ac102c820f761
Alexandrite <Alexandrite@PONKA>
2025-03-25 18:44:37 +0300

    Добавьте файлы проекта.

---
+++ Diplom2.py
@@ -0,0 +1,18 @@
+﻿import openpyxl
+#import pyautocad
+# Создаем новый Excel-файл и активный лист
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
+# Записываем данные в ячейки
+sheet['A1'] = 'Имя'
+sheet['B1'] = 'Возраст'
+sheet['A2'] = 'Алексей'
+sheet['B2'] = 25
+sheet['A3'] = 'Мария'
+sheet['B3'] = 30
+
+# Сохраняем файл
+workbook.save('example.xlsx')
+
+print("Файл 'example.xlsx' успешно создан!")
