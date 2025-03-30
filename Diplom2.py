2100c1570bdfdf50e7549e83e952516404eaede2
Ilia Kuzin <ilyakuzin20013107@gmail.com>
2025-03-30 19:42:30 +0300

    Добавление файла входных данных "ДАТА_" и шаблона чертежа "Шаблон". Из кода убран пример и сделан вывод ячейки из лста эксель

--- Diplom2.py
+++ Diplom2.py
@@ -1,18 +1,14 @@
 ﻿import openpyxl
 #import pyautocad
 # Создаем новый Excel-файл и активный лист
-workbook = openpyxl.Workbook()
-sheet = workbook.active
-#Привет!!!!!
-# Записываем данные в ячейки
-sheet['A1'] = 'Имя'
-sheet['B1'] = 'Возраст'
-sheet['A2'] = 'Алексей'
-sheet['B2'] = 25
-sheet['A3'] = 'Мария'
-sheet['B3'] = 30
 
-# Сохраняем файл
-workbook.save('example.xlsx')
+fileName="DATA_"
+add=".xlsm"
+sheetName="_ПС"
+workbook = openpyxl.load_workbook(fileName+add)
 
-print("Файл 'example.xlsx' успешно создан!")
+sheet=workbook[sheetName]
+
+value=sheet['D5'].value
+
+print(value)
