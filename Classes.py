e91dc9453d97bced8d69ba3d63b78c1aefe9923e
Ilia Kuzin <ilyakuzin20013107@gmail.com>
2025-04-02 00:31:28 +0300

    Удален лишний Excel файл и добавлены классы

---
+++ Classes.py
@@ -0,0 +1,30 @@
+class coor(object):
+    #pass
+    X:float
+    Y:float
+
+class Wall(object):
+    name: str
+    sheet_address: str
+    foundation_level: float
+    length: float
+    height_start: float
+    height_end: float
+    foundation_width: float
+    top_wall_width: float
+    edge_distance: float
+    bottom_wall_width: float
+    t1: float
+    t2: float
+    t3: float
+    t4: float
+
+class Walls(object)
+    name: str
+    total_count: int
+    sections: List[Wall]
+
+
+
+
+
