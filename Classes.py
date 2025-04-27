aea781d0ec199f8a46e772019fd4080b7efe4270
Ilia Kuzin <ilyakuzin20013107@gmail.com>
2025-04-27 18:15:56 +0300

    Реализован ввод данных из Excel(подумать как реализовать лучше) и метод вывода плей класса Wall на экран

--- Classes.py
+++ Classes.py
@@ -1,13 +1,20 @@
-﻿class coor(object):
+﻿from turtle import position
+
+
+class coor(object):
     #pass
     X:float
     Y:float
 
 class Wall(object):
     name: str # Имя стенки
+    name_wall:str
+    name_founding:str
+    count:int
+    position:float # Отметка подошвы 
     leght: float # Длина секции, м - L
     height_end: float
-    height_end: float # Привет
+    height_start: float # Привет
     foundation_width: float
     top_wall_width: float
     edge_distance: float
@@ -16,11 +23,33 @@ class Wall(object):
     t2: float
     t3: float
     t4: float
+    V1: float
+    V2: float
+
+    def ShowAll(self):
+        print("Имя секции: "+self.name)
+        print("Имя ростверка: "+self.name_founding)
+        print("Имя стенки: "+self.name_wall)
+        print("Отметка подошвы: "+str(self.position))
+        print("Число: "+str(self.count))
+        print("Длина: "+str(self.leght))
+        print("Общая высота (нач): "+str(self.height_start))
+        print("Общая высота (кон): "+str(self.height_end))
+        print("Ширина фундамента: "+str(self.foundation_width))
+        print("Ширина стены сверху : "+str(self.top_wall_width))
+        print("Расстояние от стены до края: "+str(self.edge_distance))
+        print("Ширина стены внизу: "+str(self.bottom_wall_width))
+        print("Толщина перекрытия 1 у стены: "+str(self.t1))
+        print("Толщина перекрытия 1 у насыпи: "+str(self.t2))
+        print("Толщина перекрытия 2 у стены: "+str(self.t3))
+        print("Толщина перекрытия 2 у насыпи: "+str(self.t4))
+        print("Объем ростверка подпорной стены, м3: "+str(self.V1))
+        print("Объем тела подпорной стены, м3: "+str(self.V2))
 
 class Walls(object):
     name: str
     total_count: int
-    sections: List[Wall]
+  #  sections: List[Wall]
 
 
 
--- Diplom2.py
+++ Diplom2.py
@@ -1,20 +1,42 @@
 ﻿import openpyxl
+
+from Classes import Wall
 #import pyautocad
 # Создаем новый Excel-файл и активный лист
 
-fileName="DATA_"
+file_Name="DATA_"
 add=".xlsm"
-sheetName="_ПС"
-workbook = openpyxl.load_workbook(fileName+add, data_only=True )
+sheet_Name="_ПС"
+workbook = openpyxl.load_workbook(file_Name+add, data_only=True )
 
-sheet=workbook[sheetName]
+sheet=workbook[sheet_Name]
 
-startPoint='D3'
+start_Point='D3'
+read_row=3
+read_column=4
+value=sheet.cell(row=read_row,column=read_column).value
 
-value=sheet.cell(row=3,column=4).value
+input_data=Wall()
+
+input_data.name=sheet.cell(row=read_row,column=read_column).value
+input_data.name_founding=sheet.cell(row=read_row+1,column=read_column).value
+input_data.name_wall=sheet.cell(row=read_row+3,column=read_column).value
+input_data.count=sheet.cell(row=read_row+5,column=read_column).value
+input_data.position=sheet.cell(row=read_row+6,column=read_column).value
+input_data.leght=sheet.cell(row=read_row+7,column=read_column).value
+input_data.height_start=sheet.cell(row=read_row+8,column=read_column).value
+input_data.height_end=sheet.cell(row=read_row+9,column=read_column).value
+input_data.foundation_width=sheet.cell(row=read_row+10,column=read_column).value
+input_data.top_wall_width=sheet.cell(row=read_row+11,column=read_column).value
+input_data.edge_distance=sheet.cell(row=read_row+12,column=read_column).value
+input_data.bottom_wall_width=sheet.cell(row=read_row+13,column=read_column).value
+input_data.t1=sheet.cell(row=read_row+14,column=read_column).value
+input_data.t2=sheet.cell(row=read_row+15,column=read_column).value
+input_data.t3=sheet.cell(row=read_row+16,column=read_column).value
+input_data.t4=sheet.cell(row=read_row+17,column=read_column).value
+input_data.V1=sheet.cell(row=read_row+18,column=read_column).value
+input_data.V2=sheet.cell(row=read_row+18,column=read_column).value
 
 
+input_data.ShowAll()
 
-
-
-print(value)
